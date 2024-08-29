455307d8bbf292550886feaff9fdf946306355a4
MohammadHelaly <mohammad.helaly@gmail.com>
2024-08-29 20:14:17 +0300

    Updates

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -3,6 +3,7 @@ const Background = () => {
     <div className="fixed top-0 -z-10 h-screen w-full overflow-hidden bg-space">
       <img
         src={import.meta.env.BASE_URL + "assets/images/background-image.jpg"}
+        alt="Background Image of the Moon"
         className="h-screen w-full object-cover object-center brightness-[0.8]"
       />
     </div>
