d1707b95a8247267c15f0abe8c61d473bff69db5
MohammadHelaly <mohammad.helaly@gmail.com>
2024-08-29 17:54:38 +0300

    Updates

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -1,6 +1,6 @@
 const Background = () => {
   return (
-    <div className="fixed top-0 -z-10 h-screen w-full overflow-hidden bg-muted">
+    <div className="fixed top-0 -z-10 h-screen w-full overflow-hidden bg-space">
       <img
         src={import.meta.env.BASE_URL + "assets/images/background-image.jpg"}
         className="h-screen w-full object-cover object-center brightness-[0.8]"
