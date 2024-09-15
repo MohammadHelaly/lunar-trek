cadeb6db066b5587ba26192d3510335edf1e52fd
MohammadHelaly <mohammad.helaly@gmail.com>
2024-09-15 23:12:52 +0300

    Update navigation

--- src/lib/router/routes.ts
+++ src/lib/router/routes.ts
@@ -1,8 +1,5 @@
 enum Routes {
   HOME = "/",
-  HOME_EXPLORE = "/#explore",
-  HOME_ABOUT = "/#about",
-  HOME_CONTACT = "/#contact",
   GLOBE_EXPLORATION = "/globe-exploration",
   UNREAL_ENGINE_EXPLORATION = "/unreal-engine-exploration",
   WILDCARD = "*",
