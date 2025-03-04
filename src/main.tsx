99433f4667f19b59572d5e9e3969dae4237468ba
Mohammad Helaly <mohammad.helaly@gmail.com>
2025-03-04 03:15:15 +0200

    Register service worker

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/app";
 import "@/styles/index.css";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
