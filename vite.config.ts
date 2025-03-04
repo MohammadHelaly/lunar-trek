cfaa5fbd976e84260b82a298907a49184422bcd1
Mohammad Helaly <mohammad.helaly@gmail.com>
2025-03-04 03:14:59 +0200

    Add pwa configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,50 @@ export default defineConfig({
         titleProp: true,
       },
       include: "**/*.svg",
+    }),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+      strategies: "injectManifest",
+      injectManifest: {
+        swSrc: "src/service-worker.ts",
+        swDest: "dist/service-worker.js",
+        globDirectory: "dist",
+        globPatterns: ["**/*.{js,css,html,png,jpg,svg,json,otf}"],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      },
+      srcDir: "src",
+      filename: "service-worker.ts",
+      registerType: "autoUpdate",
+      base: "/",
+      manifest: {
+        name: "Lunar Trek",
+        short_name: "Lunar Trek",
+        description: "Explore the secrets beneath the surface of Earth's moon.",
+        theme_color: "#06060c",
+        background_color: "#06060c",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/favicon.ico",
+            sizes: "64x64",
+            type: "image/x-icon",
+          },
+          {
+            src: "/logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
   server: {
